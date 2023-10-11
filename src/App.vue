fe0a9c267fe5e5cc427698cc319b7c0d617e50f5
Jackson Almeida <jackson.almeida@dismuntel.local>
2023-10-11 11:40:05 +0200

    Slider

---
+++ src/App.vue
@@ -0,0 +1,76 @@
+<script setup>
+import { authors } from "./helpers/fake_api";
+
+console.log(authors);
+</script>
+
+<template>
+  <div class="wall">
+    <h1 class="wall__title">Warriors of 20's century</h1>
+    <div class="wall__box">
+      <div
+        class="wall__box__card"
+        v-for="aut in authors"
+        :key="aut.id"
+        :style="{ backgroundImage: aut.img }"
+      >
+        <div class="wall__box__card__info">
+          <h1>{{ aut.name }}</h1>
+          <h3>{{ aut.motto }}</h3>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.wall {
+  /* background-color: lightgrey; */
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 100vh;
+  width: 100vw;
+
+  &__title {
+    color: azure;
+    display: flex;
+    justify-content: center;
+    font-size: 5em;
+  }
+
+  &__box {
+    /* background-color: black; */
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 20px;
+
+    &__card {
+      background-size: cover;
+      background-position: top;
+      filter: grayscale(1);
+
+      width: 200px;
+      height: 400px;
+      margin-inline: 5px;
+      border-radius: 20px;
+
+      transition: 1s ease-in-out;
+
+      &__info {
+        margin-left: 30px;
+        display: none;
+      }
+
+      &:hover {
+        width: 400px;
+      }
+
+      &:hover &__info {
+        display: block;
+      }
+    }
+  }
+}
+</style>
---
+++ src/helpers/fake_api.js
@@ -0,0 +1,54 @@
+
+export let authors = [
+    {
+        id: 1,
+        name: 'Jordan Peterson',
+        motto: 'Truth Seeker',
+        img: 'url(../src/assets/imgs/peterson.jpg)'
+    },
+    {
+        id: 2,
+        name: 'David Goggins',
+        motto: 'Hard motherf*ker',
+        img: 'url(../src/assets/imgs/david.jpg)'
+    },
+    {
+        id: 3,
+        name: 'Ben Shapiro',
+        motto: 'The tradition',
+        img: 'url(../src/assets/imgs/ben.jpg)'
+    },
+    {
+        id: 4,
+        name: 'Andrew Tate',
+        motto: 'Stoic Creator',
+        img: 'url(../src/assets/imgs/tate.jpg)'
+    },
+    {
+        id: 5,
+        name: 'Elon Musk',
+        motto: 'Beyond Vision',
+        img: 'url(../src/assets/imgs/elon.jpg)'
+    },
+    {
+        id: 6,
+        name: 'Khabib Nurmanomedov',
+        motto: 'Hard motherf*',
+        img: 'url(../src/assets/imgs/khabib.jpeg)'
+    },
+    {
+        id: 7,
+        name: 'Ben Shapiro',
+        motto: 'Traditionalist',
+        img: 'url(../src/assets/imgs/ben.jpg)'
+    },
+    {
+        id: 8,
+        name: 'Andrew Tate',
+        motto: 'Stoic Creator',
+        img: 'url(../src/assets/imgs/tate.jpg)'
+    },
+]
+
+
+
